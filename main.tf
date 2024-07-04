1ad7da85dc0719fca51b91d0e658b525f9165e82
nowsheene <31991137+nowsheene@users.noreply.github.com>
2024-07-04 18:16:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,18 @@ provider "aws" {
     region = "eu-north-1"  
 }
 
-resource "aws_instance" "c8.local" {
+resource "aws_instance" "centos_instance" {
   ami           = "ami-0483db98bd5d1c5d1"
-  instance_type = "t3.micro "
+  instance_type = "t3.micro"
+  tags = {
+    Name = "c8.local"
+  }
 }
 
-resource "aws_instance" "u22.local" {
+resource "aws_instance" "ubuntu_instance" {
   ami           = "ami-07a0715df72e58928"
-  instance_type = "t3.micro "
+  instance_type = "t3.micro"
+  tags = {
+    Name = "u22.local"
+  }
 }
