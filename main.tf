73521d1dc565a67bed8ef23ce36cdf4b4f8236e1
nowsheene <31991137+nowsheene@users.noreply.github.com>
2024-07-04 16:25:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "eu-north-1"  
 }
 
 resource "aws_instance" "c8.local" {
   ami           = "ami-0483db98bd5d1c5d1"
+  instance_type = "t3.micro "
+}
+
+resource "aws_instance" "u22.local" {
+  ami           = "ami-07a0715df72e58928"
   instance_type = "t3.micro "
 }
